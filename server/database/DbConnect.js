468da5882355e882d00b32d310dfb9e69e19b689
Anuj Bansal <115884905+Anujbansal527@users.noreply.github.com>
2024-01-09 15:23:39 +0530

    Update DbConnect.js

--- server/database/DbConnect.js
+++ server/database/DbConnect.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const DbConnect = () =>{
-    mongoose.connect(process.env.LOCAL_DB_URL,{ })
+    mongoose.connect(process.env.DB_URL,{ })
     .then(()=> console.log("MongoDB Connected..."))
     .catch((error) => {console.log(`error occured while connecting db ${error.message}`)})
 }
